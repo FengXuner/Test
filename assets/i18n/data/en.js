c1e1ea87ed4382a344189a61cdf55717ae8921bd
FengXuner <gameprogramer@126.com>
2018-11-08 10:05:06 +0800

    【多语言】

---
+++ assets/i18n/data/en.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "head": 'head',
+	"body": 'body',
+	"leftHand": 'leftHand',
+	"rightHand": 'rightHand',
+	"leftFoot": 'leftFoot',
+	"rightFoot": 'rightFoot',
+};
---
+++ assets/i18n/data/zh.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "head": '头',
+	"body": '身体',
+	"leftHand": '左手',
+	"rightHand": '右手',
+	"leftFoot": '左脚',
+	"rightFoot": '右脚',
+};
